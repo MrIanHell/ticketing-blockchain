9dc391a92441078586e04fe79c2e9b072c9f8647
MrIanHell <ianhell95@gmail.com>
2019-02-13 20:56:58 +0000

    Adding a transfer function and event

--- Token/contracts/TicketToken.sol
+++ Token/contracts/TicketToken.sol
@@ -1,17 +1,34 @@
 pragma solidity ^0.4.2;
 
 contract TicketToken{
-
 	// state variables (variables accessible to the entire contract and written to disk on the blockchain)
 	string public name = 'Ticket Token'; 
 	string public symbol = 'TCK';
 	string public standard = 'Ticket Token v1.0';
-	uint256 public totalSupply; 
-
+	uint256 public totalSupply;
+	
 	mapping(address => uint256) public balanceOf;
+
+	event Transfer( address indexed _from, address indexed _to, uint256 _value);
 
 	constructor(uint256 _initialSupply) public { // convention to use underscores for local variables
 		balanceOf[msg.sender] = _initialSupply;
 		totalSupply = _initialSupply;
+	}
+
+
+	function transfer(address _to, uint256 _value) public returns(bool success) {
+		// Raise exception if account doesn't have sufficient funds
+		require(balanceOf[msg.sender] >= _value); 
+
+		// Transfer balance
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] += _value;
+
+		// Triggers the Transfer Event
+		emit Transfer(msg.sender, _to, _value);
+
+		// Returns a true boolean if all of the code above executes successfully
+		return true;
 	}
 }
--- Token/test/TicketToken.js
+++ Token/test/TicketToken.js
@@ -2,6 +2,7 @@ var TicketToken = artifacts.require("./TicketToken.sol")
 
 contract('TicketToken', function(accounts) {
 	var tokenInstance
+	var account1 = web3.eth.accounts.create().address
 
 	it('initialises the contract with the correct values', function(){
 		return TicketToken.deployed().then(function(instance) {
@@ -27,6 +28,31 @@ contract('TicketToken', function(accounts) {
 			return tokenInstance.balanceOf(accounts[0]);
 		}).then(function(adminBalance) {
 			assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin account')
+		})
+	})
+
+	it('transfers token ownership', function() {
+		return TicketToken.deployed().then(function(instance) {
+			tokenInstance = instance
+			return tokenInstance.transfer.call(account1, 1000000000)
+		}).then(assert.fail).catch(function(error) {
+			assert(error.message.indexOf('run Out of Gas') >= 0, 'error message must contain run out of gas')
+			return tokenInstance.transfer.call(account1, 300000, { from: accounts[0] })
+		}).then(function(success) {
+			assert.equal(success, true, 'it returns true')
+			return tokenInstance.transfer(account1, 300000, { from: accounts[0] })
+		}).then(function(receipt){
+			assert.equal(receipt.logs.length, 1, 'triggers one event')
+			assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event')
+			assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from')
+			assert.equal(receipt.logs[0].args._to, account1, 'logs the account the tokens are transferred to')
+			assert.equal(receipt.logs[0].args._value, 300000, 'logs the transfer amount')
+			return tokenInstance.balanceOf(account1)
+		}).then(function(balance) {
+			assert.equal(balance.toNumber(), 300000, 'adds the amount to the receiving account')
+			return tokenInstance.balanceOf(accounts[0])
+		}).then(function(balance) {
+			assert.equal(balance.toNumber(), 700000, 'deducts the amount from the sending account')
 		})
 	})
 })
