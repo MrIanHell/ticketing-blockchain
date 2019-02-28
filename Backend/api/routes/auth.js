485554eebee76579a1c675257d80232407616736
MrIanHell <ianhell95@gmail.com>
2019-02-28 22:28:03 +0000

    Adding a login route to auth with JWT tokens

--- Backend/api/routes/auth.js
+++ Backend/api/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const router = express.Router()
 
@@ -35,6 +36,39 @@ router.post('/signup', (req, res, next) => {
                 })
             })
         }
+    })
+})
+
+router.post('/login', (req, res, next) => {
+    User.findOne({ email: req.body.email }).exec().then(user => {
+        if(!user){
+            return res.status(401).json({
+                message: 'Authentication failed'
+            })
+        }
+        bcrypt.compare(req.body.password, user.password, (err, result) => {
+            if(!result){
+                return res.status(401).json({
+                    message: 'Authentication failed'
+                })
+            }
+            if(result){
+                const token = jwt.sign({
+                    email: user.email,
+                    userId: user._id
+                }, process.env.JWT_KEY, { expiresIn: "1h" })
+                return res.status(200).json({
+                    message: 'Authentication was successful',
+                    token: token
+                })
+            }
+        })
+
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
     })
 })
 
